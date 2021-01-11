c120c952efffaf481062e803b1f9a1c626af3c3c
darwishzainstudio <darwishzainstudio@gmail.com>
2021-01-11 22:32:01 +0800

    11012021 2231

---
+++ SaviourRobot1/Assets/Script/toLeft.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toLeft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left);
+        if(transform.position.x<=-10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
